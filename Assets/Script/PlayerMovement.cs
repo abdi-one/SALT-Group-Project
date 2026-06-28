a79e5f80da289a271d05feb990e4633d4c7ec6de
abdi-one <abdilahi.b.abdi@gmail.com>
2026-06-28 18:40:30 +1000

    added Sai's files

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //code for adjusting game logic or changing variable
     [Header("Movement Parameters")]
     [SerializeField] private float speed;
     [SerializeField] private float jumpPower;
@@ -17,33 +16,35 @@ public class PlayerMovement : MonoBehaviour
     
     [Header("Layer")]
     [SerializeField] private LayerMask groundLayer;
-
-    //code for getting Unity components
+    
     private Rigidbody2D body;
+    private Animator anim;
     private BoxCollider2D boxCollider;
-    private float horizontalInput;
 
     private void Awake()
     {
+        //grab references
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        
         //player movement
+        float horizontalInput =  Input.GetAxis("Horizontal");
         body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
-
-        //flipping the player direction via transform
-        if (horizontalInput > 0.01f)
+        
+        //flip player when moving left-right
+        if(horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
-
         
-        
+        //set animator parameters
+            anim.SetBool("walk", horizontalInput != 0);
+            anim.SetBool("grounded",  isGrounded());
+            
         //player jump logic
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
@@ -64,13 +65,6 @@ public class PlayerMovement : MonoBehaviour
         {
             coyoteCounter -= Time.deltaTime;
         }
-    }
-
-    //check if player is on ground
-    private bool isGrounded()
-    {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
     }
 
     private void Jump()
@@ -94,5 +88,11 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
         }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 }
