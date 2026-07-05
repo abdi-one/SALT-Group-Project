79da14c44c521cfad4dbd65eea3f82a7beab2eb9
SaiPhomma <saiphomma1@gmail.com>
2026-07-05 22:20:49 +0700

    character resprite

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -39,9 +39,9 @@ public class PlayerMovement : MonoBehaviour
 
         // flip player when moving left-right
         if (horizontalInput > 0.01f)
-            transform.localScale = Vector3.one;
+            transform.localScale = new Vector3(6, 6, 6);
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-6, 6, 6);
 
         // set animator parameters
         anim.SetBool("walk", horizontalInput != 0);
